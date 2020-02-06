2a801cf329163bd58e9581945e658b4a56bc4b76
Austin Rich <austin.rich@e-hps.com>
2020-02-06 06:36:10 -0500

    Uncomment code

--- app/static/client.js
+++ app/static/client.js
@@ -18,32 +18,53 @@ function analyze(model) {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  // el("analyze-button").innerHTML = "Analyzing...";
-  // var xhr = new XMLHttpRequest();
-  // var loc = window.location;
-  // xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze/` + window.mlModel,
-  //   true);
-  // xhr.onerror = function() {
-  //   alert(xhr.responseText);
-  // };
-  // xhr.onload = function(e) {
-  //   if (this.readyState === 4) {
-  //     var response = JSON.parse(e.target.responseText);
-  //     el("result-label").innerHTML = `Result = ${response["result"]}`;
-  //   }
-  //   el("analyze--button").innerHTML = "Go Go Gadget Analyze";
-  // };
-  //
-  // var fileData = new FormData();
-  // fileData.append("file", uploadFiles[0]);
-  // xhr.send(fileData);
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze/` + window.mlModel,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    }
+    el("analyze--button").innerHTML = "Go Go Gadget Analyze";
+  };
 
-  el("result-label").innerHTML = `Result = ${response["result"]}`;
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
 }
 
 function yieldResult(result) {
-  let beginning = 'Hmmmm. I think I\'s'
-  switch(rand) {
-
+  let beginning = 'Hmmmm. I\'m resonably certain It\'s an image of ';
+  let confidence = 1;
+  if (confidence > 0.6) {
+    switch(rand) {
+      case 1:
+        beginning = 'Oh. This is ';
+        break;
+      case 2:
+        beginning = 'You\'re looking at ';
+        break;
+      case 3:
+        beginning = 'That\'s ';
+        break;
+    }
+  } else {
+    switch(rand) {
+      case 1:
+        beginning = 'Hm. I think it\'s ';
+        break;
+      case 2:
+        beginning = 'I\'m somewhat confident its ';
+        break;
+      case 3:
+        beginning = 'Reasonably certain this is ';
+        break;
+    }
   }
 }
