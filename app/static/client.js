5a30d0375046a7eb286cec2738b87a3f60ef34d2
Austin Rich <austin.rich@e-hps.com>
2020-02-06 07:26:40 -0500

    Final updates...assuming the result text works

--- app/static/client.js
+++ app/static/client.js
@@ -1,4 +1,4 @@
-var el = x => document.getElementById(x);
+let el = x => document.getElementById(x);
 
 function showPicker() {
   el("file-input").click();
@@ -6,7 +6,7 @@ function showPicker() {
 
 function showPicked(input) {
   el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
+  let reader = new FileReader();
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
@@ -15,12 +15,19 @@ function showPicked(input) {
 }
 
 function analyze(model) {
-  var uploadFiles = el("file-input").files;
+  let uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+  debugger;
+  let testResponse = {"result":"baseball","resultIndex":"tensor(0)","resultConfidence":"tensor([1.0000e+00, 1.4616e-06])"};
+  let regExp = /\(([^)]+)\)/;
+  let matches = regExp.exec(testResponse.resultIndex);
+  testResponse.resultIndex = parseInt(matches[1], 10);
+  matches = regExp.exec(testResponse.resultConfidence);
+  testResponse.resultConfidence = JSON.parse(matches[1]);
 
   el("analyze-button").innerHTML = "Analyzing...";
-  var xhr = new XMLHttpRequest();
-  var loc = window.location;
+  let xhr = new XMLHttpRequest();
+  let loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze/` + window.mlModel,
     true);
   xhr.onerror = function() {
@@ -28,21 +35,26 @@ function analyze(model) {
   };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
-      var response = JSON.parse(e.target.responseText);
-
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      let response = JSON.parse(e.target.responseText);
+      let regExp = /\(([^)]+)\)/;
+      let matches = regExp.exec(response.resultIndex);
+      response.resultIndex = parseInt(matches[1], 10);
+      matches = regExp.exec(response.resultConfidence);
+      response.resultConfidence = JSON.parse(matches[1]);
+      yieldResult(response)
     }
     el("analyze--button").innerHTML = "Go Go Gadget Analyze";
   };
 
-  var fileData = new FormData();
+  let fileData = new FormData();
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
 
-function yieldResult(result, resultIndex, resultConfidence) {
+function yieldResult(response) {
   let beginning = 'Hmmmm. I\'m resonably certain It\'s an image of ';
-  let confidence = 1;
+  let confidence = response.resultConfidence[response.resultIndex];
+  let rand = Math.floor(Math.random() * 3) + 1
   if (confidence > 0.6) {
     switch(rand) {
       case 1:
@@ -67,5 +79,14 @@ function yieldResult(result, resultIndex, resultConfidence) {
         beginning = 'Reasonably certain this is ';
         break;
     }
+    if(window.mlModel === 'Baseball' && window.enableResultTheme) {
+      if (response.result === 'baseball') {
+        themes.baseball()
+      } else {
+        themes.cricket()
+      }
+    }
+
+    el("result-label").innerHTML = beginning + response.result.toLowerCase();
   }
 }
